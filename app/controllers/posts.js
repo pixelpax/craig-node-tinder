3f18d4720b6fed8d54659cb5483f867d9f18ba63
Gabriel Elkind <gwelkind@gmail.com>
2018-04-14 14:07:21 -0500

    Creates paging on the frontend

--- app/controllers/posts.js
+++ app/controllers/posts.js
@@ -5,14 +5,26 @@ export default Ember.Controller.extend({
   store: Ember.inject.service(),
 
   posts: [],
+  currentPage: 1,
 
   async _updateList() {
-    let newPosts = await this.store.query('post', {pageNumber: 1, pageSize: 3});
+    let currentPage = this.get('currentPage');
+    let newPosts = await this.store.query('post', {pageNumber: currentPage, pageSize: 3});
     this.set('posts', newPosts);
   },
 
   actions: {
     async updateList() {
+      return this._updateList();
+    },
+    async nextPage() {
+      let lastPage = this.get('currentPage');
+      this.set('currentPage', lastPage + 1);
+      return this._updateList();
+    },
+    async prevPage() {
+      let lastPage = this.get('currentPage');
+      this.set('currentPage', lastPage - 1);
       return this._updateList();
     }
   }
